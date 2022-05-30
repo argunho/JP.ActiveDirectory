1ebadccbaa69c886196f12af8f87a2686a9d965e
argunho <aslan_argun@hotmail.com>
2022-05-30 22:08:33 +0200

    update

--- ActivDerictory/Repository/MailRepository.cs
+++ ActivDerictory/Repository/MailRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -11,28 +12,29 @@ namespace ActiveDirectory.Repositories;
 public class MailRepository
 {
     // Template params
-    public static string mailHtml = "<!DOCTYPE html><html><body>" +
-                                    "<div style=\"width:98%;display:block;margin:auto;background:#FFFFFF;font-family:Tahoma;\">" +
-                                        "<div style=\"display:block;width:auto;height:max-content;padding:25px 0;overflow:hidden\">" +
-                                             "<img style=\"width:70px;margin:30px\" src='https://www.alvesta.se/assets/images/framework/Alvesta.svg' alt='Alvesta Kommun'/>" +
-                                        "</div>" +
-                                        "<div style=\"width:auto;padding:35px;font-size:14px;display:block;\">{content}</div>" +
+    public static string mailHtml = "<div style=\"width:98%;display:block;margin:auto;background-color:#FFFFFF;font-family:Tahoma;\">" +
+                                        "<div style=\"width:100%;height:140pt;display:block;background:#1F722F;color:#FFF;font-size:30px\">" +
+                                            "<img src=\"data:image/png;base64,{logo}\" alt='Alvesta Kommun' width='200' height='100' style=\"display: block;margin-left: 30px\"/></div>" +
+                                        "<div style=\"width:auto;padding: 35px 25px;font-size:'14px';display:block;\">{content}</div>" +
                                         "<div style=\"width:96%;margin:20px 1%;display:block;padding:25px 1%;text-align:center;line-height:25px;font-size:16px;border-top:1px solid #D8D8D8;display:flex;justify-content:center;\">" +
-                                            "<div style=\"width:50%;min-width:320px;display:block;margin:auto;font-family:Franklin Gothic Medium;\">{footer}</div>" +
-                                    "</div></div></body></html>";
+                                            "<div style=\"width:50%;min-width:320px;display:block;margin:auto;font-family:Franklin Gothic Medium;\">" +
+                                            "</div></div></div>";
 
     // Send mail service
     public bool SendMail(string toEmail, string mailSubject, string mailContent, IFormFile? attachedFile = null)
     {
         try
         {
-            MailMessage _mail = new MailMessage(new MailAddress("no.reply@alvesta.se", "Unlock User"), new MailAddress(toEmail));
+            var logo = ImageToBase64(@"wwwroot/alvestakommun.png");
+            MailMessage _mail = new MailMessage(new MailAddress("no-reply@alvesta.se", "Unlock User"), new MailAddress(toEmail));
             _mail.Subject = mailSubject;
-            _mail.Body = mailHtml.Replace("{content}", mailContent).Replace("{footer}", "Alvesta Kommun");
+            _mail.Body = mailHtml.Replace("{content}", mailContent).Replace("{logo}", logo);
             _mail.IsBodyHtml = true;
             if (attachedFile != null)
-                _mail.Attachments.Add(new Attachment(attachedFile.OpenReadStream(), mailSubject + "." 
+            {
+                _mail.Attachments.Add(new Attachment(attachedFile.OpenReadStream(), mailSubject + "."
                         + attachedFile.ContentType.Substring(attachedFile.ContentType.IndexOf("/") + 1)));
+            }
 
             SmtpClient _smtp = new SmtpClient();
             _smtp.Host = "smtp.alvesta.local";
@@ -55,4 +57,22 @@ public class MailRepository
             return false;
         }
     }
+
+    #region Help methods
+    public string ImageToBase64(string imgUrl = "")
+    {
+        string imgBase64 = "";
+        using (Image img = Image.FromFile(imgUrl))
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                Image imageToConvert = img;
+                imageToConvert.Save(m, img.RawFormat);
+                byte[] imageBytes = m.ToArray();
+                imgBase64 = Convert.ToBase64String(imageBytes);
+            }
+        }
+        return imgBase64;
+    }
+    #endregion
 }
